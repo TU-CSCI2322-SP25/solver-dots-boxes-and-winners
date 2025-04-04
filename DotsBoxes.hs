70b5ccd8b2f4c7e47d549bfe08c57447ec06d37c
Gabriel Garza <144834843+ggarza3@users.noreply.github.com>
2025-04-04 14:19:52 -0500

    Create DotsBoxes.hs
    defined basic data types

---
+++ DotsBoxes.hs
@@ -0,0 +1,15 @@
+type Point = (Int, Int)
+type Player = Char
+type Order = [Player]
+type Box = (Point, Player)
+data Dir = Vertical | Horizontal deriving (Show, Eq)
+type Line = (Point, Dir)
+type Move = Line
+type Grid = [Line]
+data Board = Board { size :: (Int, Int), grid :: Grid, boxes :: [Box], order :: Order}
+
+showCycle (x:xs) = show (x:(takeWhile (/=x) xs))
+instance Show Board where
+  show (Bd gr pt ord) = unword ["Board", show gr, show pt, showCycle ord]
+
+data Winner = Player | None | Tie String
