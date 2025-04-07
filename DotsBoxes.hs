6f21909c2613b5fcfe8a73bdddbf251087d25763
MeraOPDF <cayoford@gmail.com>
2025-04-07 15:58:49 -0500

    first attempt at a solution

--- DotsBoxes.hs
+++ DotsBoxes.hs
@@ -15,3 +15,17 @@ showCycle (x:xs) = show (x:(takeWhile (/=x) xs))
   --show (Bd gr pt ord) = unword ["Board", show gr, show pt, showCycle ord]
 
 data Winner = Win Player | None | Tie String
+
+
+
+dividePoints :: [Player] -> [Box] -> [(Player, [Box])]
+dividePoints [] bxs = []
+dividePoints (pl:pls) bxs = (pl, [x| x<-bxs, (snd x) == pl]):(dividePoints pls bxs)
+
+winnerFromPoints :: [(Player, [Box])] -> Winner
+winnerFromPoints pts = if length trimmed == 1 then Win (head trimmed) else (Tie trimmed)
+  where mst = maximum [length y| (x,y) <- pts]
+        trimmed = [x| (x,y) <- pts, length y == mst]
+
+findWinner :: Board -> Winner
+findWinner bd = if length (boxes bd) < ((fst (size bd)) - 1) * ((snd (size bd)) - 1) then None else winnerFromPoints (dividePoints (show (order bd)) (boxes bd))
