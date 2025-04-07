d1787a132aa91bbfc0aaf8328ec92eee270cb92a
MeraOPDF <157915160+MeraOPDF@users.noreply.github.com>
2025-04-07 15:43:03 -0500

    slight tweak to make the winner type properly useable

--- DotsBoxes.hs
+++ DotsBoxes.hs
@@ -14,4 +14,4 @@ showCycle (x:xs) = show (x:(takeWhile (/=x) xs))
 --instance Show Board where
   --show (Bd gr pt ord) = unword ["Board", show gr, show pt, showCycle ord]
 
-data Winner = Player | None | Tie String
+data Winner = Win Player | None | Tie String
