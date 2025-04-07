ebedf4fff06c60db34225cdb71fbd8b2415c47c9
Gabriel Garza <144834843+ggarza3@users.noreply.github.com>
2025-04-07 13:59:48 -0500

    Update DotsBoxes.hs

--- DotsBoxes.hs
+++ DotsBoxes.hs
@@ -1,4 +1,5 @@
-module DotsBoxes
+module DotsBoxes where
+
 type Point = (Int, Int)
 type Player = Char
 type Order = [Player]
@@ -10,7 +11,7 @@ type Grid = [Line]
 data Board = Board { size :: (Int, Int), grid :: Grid, boxes :: [Box], order :: Order}
 
 showCycle (x:xs) = show (x:(takeWhile (/=x) xs))
-instance Show Board where
-  show (Bd gr pt ord) = unword ["Board", show gr, show pt, showCycle ord]
+--instance Show Board where
+  --show (Bd gr pt ord) = unword ["Board", show gr, show pt, showCycle ord]
 
 data Winner = Player | None | Tie String
