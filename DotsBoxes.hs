a9df142433efd277172aa4e7cc6ad32f4b50248b
Gabriel Garza <ggarza1@trinity.edu>
2025-04-13 20:09:03 -0500

    made but not tested

--- DotsBoxes.hs
+++ DotsBoxes.hs
@@ -15,3 +15,17 @@ showCycle (x:xs) = show (x:(takeWhile (/=x) xs))
   --show (Bd gr pt ord) = unword ["Board", show gr, show pt, showCycle ord]
 
 data Winner = Player | None | Tie String
+
+addBox :: Move -> Board -> Player -> [Box]
+addBox ((x, y), d) b p = (boxes b) ++ vertL ++ vertR ++ horzT ++ horzB
+        where vertL = if d == Vertical && ((x+1, y), Vertical) `elem` (grid b) && ((x, y), Horizontal) `elem` (grid b) && ((x,y+1), Horizontal) `elem` (grid b) then [((x, y), p)] else []
+              vertR = if d == Vertical && ((x-1, y), Vertical) `elem` (grid b) && ((x-1, y), Horizontal) `elem` (grid b) && ((x-1,y+1), Horizontal) `elem` (grid b) then [((x-1, y), p)] else []
+              horzT = if d == Horizontal && ((x, y+1), Horizontal) `elem` (grid b) && ((x, y), Vertical) `elem` (grid b) && ((x+1,y), Vertical) `elem` (grid b) then [((x, y), p)] else []
+              horzB = if d == Horizontal && ((x, y-1), Horizontal) `elem` (grid b) && ((x, y-1), Vertical) `elem` (grid b) && ((x+1,y-1), Vertical) `elem` (grid b) then [((x, y), p)] else []
+
+makeMove :: Board -> Move -> Board
+makeMove b m =
+        let nGrid = (grid b) ++ [m]
+            nBoxes = addBox m b (head (order b))
+        in Board { size = (size b), grid = nGrid, boxes = nBoxes, order = (order b)} 
+            
